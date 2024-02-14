934fa680713325a63a449f354baf6da38465afb3
chauncie <chauncie6@gmail.com>
2024-02-14 09:35:57 +0800

    hi

--- week0/hello.cpp
+++ week0/hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
  int count = 1;
-    while (count <122)
+    while (count <0x121)
             {
                 cout<< "Hello EEE 121" << endl;
                 count +=1;
